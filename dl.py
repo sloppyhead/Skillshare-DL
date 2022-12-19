7da8e694120794fcaf98df78af3c3e7fcc45aeec
sloppyhead <blogging.varma@gmail.com>
2022-12-19 11:53:32 +0700

    Update dl.py

--- dl.py
+++ dl.py
@@ -1,12 +1,13 @@
 import sys, os
 from skillshare import Skillshare, splash
-from magic import cookie
+
+
 
 # or by class ID:
 # dl.download_course_by_class_id(189505397)
 
 def main():
-    dl = Skillshare(cookie)
+    dl = Skillshare("PHPSESSID=9c34455ca27b9036e13d02696024c04b")
     course_url = sys.argv[1]
     dl.download_course_by_url(course_url)
 
@@ -14,3 +15,4 @@ def main():
 if __name__ == "__main__":
     splash()
     main()
+
